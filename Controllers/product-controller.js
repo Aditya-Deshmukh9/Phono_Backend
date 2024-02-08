1cd360ab5a01088b9b27f069f94f13e30504f38a
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-02-08 14:15:10 +0530

    product data, limit and page add

--- Controllers/product-controller.js
+++ Controllers/product-controller.js
@@ -2,14 +2,21 @@ const Products = require("../models/products-model");
 
 const products = async (req, res, next) => {
   try {
-    const response = await Products.find();
-    if (!response) {
-      res.status(404).json({ msg: "No service found" });
-      return;
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+
+    const skip = (page - 1) * limit;
+
+    const response = await Products.find().skip(skip).limit(limit);
+
+    if (!response || response.length === 0) {
+      return res.status(404).json({ msg: "No products found" });
     }
-    res.status(200).json({ response });
+
+    res.status(200).json({ response, total: response.length });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ msg: "Internal server error" });
   }
 };
 
